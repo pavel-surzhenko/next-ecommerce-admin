53170936814ece61c9d1d38d58e056645bd45b2f
Pavlo Surzhenko <pavel.surzhenko@icloud.com>
2023-05-22 12:51:54 +0300

    add img host and main page

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 'use client';
-import { useSession, signIn, signOut } from 'next-auth/react';
+import { useSession, signIn } from 'next-auth/react';
 import Nav from './components/Nav';
+import Image from 'next/image';
 
 export default function Home() {
     const { data: session } = useSession();
@@ -23,8 +24,19 @@ export default function Home() {
     return (
         <>
             <Nav />
-            <div className='bg-white flex-grow mt-2 mr-2 rounded-lg mb-2'>
-                logged in {session.user?.email}
+            <div className='bg-white flex-grow mt-2 mr-2 rounded-lg mb-2 p-2'>
+                <div className='text-blue-900 flex justify-between'>
+                    <h2>Hello, <b>{session?.user?.name}</b></h2>
+                    <div className='flex bg-gray-300 text-black gap-1 rounded-lg overflow-hidden'>
+                        <Image
+                            src={session?.user?.image || '/'}
+                            alt='avatar'
+                            width={24}
+                            height={24}
+                        />
+                        <span className='px-2'>{session?.user?.name}</span>
+                    </div>
+                </div>
             </div>
         </>
     );
